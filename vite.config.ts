18b35794e81cac43eb8743ca9a1780c0e1f12464
Sam Denty <sam@samdenty.com>
2024-10-07 14:54:42 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ function chrome129IssuePlugin() {
           if (version === 129) {
             res.setHeader('content-type', 'text/html');
             res.write(
-              '<body><h1>Please use Chrome Canary for testing.</h1><p>Chrome 129 has an issue with JavaScript modules & vite local development, see <a href="https://github.com/stackblitz/bolt.new/issues/86#issuecomment-2395519258">here for more.</a></p><p><b>Note:</b> This only impacts <u>local development</u>. `pnpm run build` and `pnpm run start` will work fine in this browser though.</p></body>',
+              '<body><h1>Please use Chrome Canary for testing.</h1><p>Chrome 129 has an issue with JavaScript modules & vite local development, see <a href="https://github.com/stackblitz/bolt.new/issues/86#issuecomment-2395519258">here for more.</a></p><p><b>Note:</b> This only impacts <u>local development</u>. `pnpm run build` and `pnpm run start` will work fine in this browser.</p></body>',
             );
             res.end();
 

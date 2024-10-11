ef45e66b174d5949c05f0729de14212083258130
zyh <zyh320888@vip.qq.com>
2024-10-11 11:54:13 +0000

    feat: 更新了@babel包中的多个依赖包版本，包括@babel/helper-create-class-features-plugin、@babel/helper-environment-visitor等，以适应新版本的API和功能。

--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -1,8 +1,10 @@
 import { createAnthropic } from '@ai-sdk/anthropic';
-
+import { env } from 'node:process';
 export function getAnthropicModel(apiKey: string) {
+  const baseURL = env.ANTHROPIC_BASE_URL;
   const anthropic = createAnthropic({
     apiKey,
+    baseURL,
   });
 
   return anthropic('claude-3-5-sonnet-20240620');

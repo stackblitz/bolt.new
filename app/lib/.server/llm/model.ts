ecfa26102825d12a0711cf0ae7ffe2c3ea6ed1b2
shouldnotappearcalm <1191465097@qq.com>
2024-10-13 13:30:50 +0800

    feat(): support base url and model custom

--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -1,9 +1,28 @@
 import { createAnthropic } from '@ai-sdk/anthropic';
+import { getAPIKey } from '~/lib/.server/llm/api-key';
+import { env } from 'node:process';
 
-export function getAnthropicModel(apiKey: string) {
+export function getAnthropicModel(env: Env) {
   const anthropic = createAnthropic({
-    apiKey,
+    apiKey: getAPIKey(env),
+    baseURL: getBaseUrl(env) 
   });
+  return anthropic(getModel(env));
+}
 
-  return anthropic('claude-3-5-sonnet-20240620');
+export function getBaseUrl(cloudflareEnv: Env) {
+  /**
+   * The `cloudflareEnv` is only used when deployed or when previewing locally.
+   * In development the environment variables are available through `env`.
+   */
+  return env.ANTHROPIC_BASE_URL || cloudflareEnv.ANTHROPIC_BASE_URL;
+}
+
+
+export function getModel(cloudflareEnv: Env) {
+  /**
+   * The `cloudflareEnv` is only used when deployed or when previewing locally.
+   * In development the environment variables are available through `env`.
+   */
+  return env.ANTHROPIC_MODEL || cloudflareEnv.ANTHROPIC_MODEL || 'claude-3-5-sonnet-20240620';
 }
--- app/lib/.server/llm/stream-text.ts
+++ app/lib/.server/llm/stream-text.ts
@@ -1,5 +1,4 @@
 import { streamText as _streamText, convertToCoreMessages } from 'ai';
-import { getAPIKey } from '~/lib/.server/llm/api-key';
 import { getAnthropicModel } from '~/lib/.server/llm/model';
 import { MAX_TOKENS } from './constants';
 import { getSystemPrompt } from './prompts';
@@ -23,7 +22,7 @@ export type StreamingOptions = Omit<Parameters<typeof _streamText>[0], 'model'>;
 
 export function streamText(messages: Messages, env: Env, options?: StreamingOptions) {
   return _streamText({
-    model: getAnthropicModel(getAPIKey(env)),
+    model: getAnthropicModel(env),
     system: getSystemPrompt(),
     maxTokens: MAX_TOKENS,
     headers: {

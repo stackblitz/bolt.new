8701be2f6d183de4871602153b2f95e2225a950c
jonathan DS <jonathands@gmail.com>
2024-10-15 18:47:35 -0300

    added Google Generative AI (gemini) integration

--- app/lib/.server/llm/api-key.ts
+++ app/lib/.server/llm/api-key.ts
@@ -7,11 +7,14 @@ export function getAPIKey(cloudflareEnv: Env, provider: string) {
    * The `cloudflareEnv` is only used when deployed or when previewing locally.
    * In development the environment variables are available through `env`.
    */
+
   switch (provider) {
     case 'Anthropic':
       return env.ANTHROPIC_API_KEY || cloudflareEnv.ANTHROPIC_API_KEY;
     case 'OpenAI':
       return env.OPENAI_API_KEY || cloudflareEnv.OPENAI_API_KEY;
+    case 'Google':
+      return env.GOOGLE_GENERATIVE_AI_API_KEY || cloudflareEnv.GOOGLE_GENERATIVE_AI_API_KEY;
     case 'Groq':
       return env.GROQ_API_KEY || cloudflareEnv.GROQ_API_KEY;
     case 'OpenRouter':
--- app/lib/.server/llm/model.ts
+++ app/lib/.server/llm/model.ts
@@ -3,6 +3,7 @@
 import { getAPIKey } from '~/lib/.server/llm/api-key';
 import { createAnthropic } from '@ai-sdk/anthropic';
 import { createOpenAI } from '@ai-sdk/openai';
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
 import { ollama } from 'ollama-ai-provider';
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 
@@ -20,6 +21,14 @@ export function getOpenAIModel(apiKey: string, model: string) {
   });
 
   return openai(model);
+}
+
+export function getGoogleModel(apiKey: string, model: string) {
+  const google = createGoogleGenerativeAI(
+    apiKey,
+  );
+
+  return google(model);
 }
 
 export function getGroqModel(apiKey: string, model: string) {
@@ -45,7 +54,8 @@ export function getOpenRouterModel(apiKey: string, model: string) {
 
 export function getModel(provider: string, model: string, env: Env) {
   const apiKey = getAPIKey(env, provider);
-  
+
+
   switch (provider) {
     case 'Anthropic':
       return getAnthropicModel(apiKey, model);
@@ -55,6 +65,8 @@ export function getModel(provider: string, model: string, env: Env) {
       return getGroqModel(apiKey, model);
     case 'OpenRouter':
       return getOpenRouterModel(apiKey, model);
+    case 'Google':
+      return getGoogleModel(apiKey, model)
     default:
       return getOllamaModel(model);
   }

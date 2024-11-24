6e94d20a744b7726fb56994f94c3753d354fbd6a
eduardruzga <wonderwhy.er@gmail.com>
2024-11-24 15:28:49 +0200

    Couple of bugfixes

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -365,7 +365,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                           </>
                         )}
                       </IconButton>
-                      <ClientOnly>{() => <ExportChatButton exportChat={exportChat} />}</ClientOnly>
+                      {chatStarted && <ClientOnly>{() => <ExportChatButton exportChat={exportChat} />}</ClientOnly>}
                     </div>
                     {input.length > 3 ? (
                       <div className="text-xs text-bolt-elements-textTertiary">
--- app/components/chat/ExportChatButton.tsx
+++ app/components/chat/ExportChatButton.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export const ExportChatButton = ({ exportChat }: { exportChat?: () => void }) => {
   return (
     <WithTooltip tooltip="Export Chat">
-      <IconButton title="Export Chat" onClick={exportChat}>
+      <IconButton title="Export Chat" onClick={() => exportChat?.()}>
         <div className="i-ph:download-simple text-xl"></div>
       </IconButton>
     </WithTooltip>

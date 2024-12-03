b9575c8a55492669c5f447558fadbcb77a121d28
Anirban Kar <thecodacus@gmail.com>
2024-12-04 00:33:22 +0530

    fixed action-runner linting

--- app/lib/runtime/action-runner.ts
+++ app/lib/runtime/action-runner.ts
@@ -101,8 +101,9 @@ export class ActionRunner {
         console.error('Action failed:', error);
       });
 
-    // eslint-disable-next-line consistent-return -- TODO: fix this consistent-return error
-    return this.#currentExecutionPromise;
+    await this.#currentExecutionPromise;
+
+    return;
   }
 
   async #executeAction(actionId: string, isStreaming: boolean = false) {

b26e338b0bb82b6b1ead8d8a81231648adfe7ac2
Anirban Kar <thecodacus@gmail.com>
2024-12-06 23:23:17 +0530

    fix bundling

--- app/components/chat/Artifact.tsx
+++ app/components/chat/Artifact.tsx
@@ -51,7 +51,9 @@ export const Artifact = memo(({ messageId }: ArtifactProps) => {
 
     if (actions.length !== 0 && artifact.type === 'bundled') {
       const finished = !actions.find((action) => action.status !== 'complete');
-      setAllActionFinished(finished);
+      if(allActionFinished!==finished){
+        setAllActionFinished(finished);
+      }
     }
   }, [actions]);
 

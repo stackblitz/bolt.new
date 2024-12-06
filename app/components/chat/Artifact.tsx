8f6d8d8e20b2a7edf0a2468e900c415c47ee46ab
Anirban Kar <thecodacus@gmail.com>
2024-12-06 23:23:32 +0530

    linting

--- app/components/chat/Artifact.tsx
+++ app/components/chat/Artifact.tsx
@@ -51,7 +51,8 @@ export const Artifact = memo(({ messageId }: ArtifactProps) => {
 
     if (actions.length !== 0 && artifact.type === 'bundled') {
       const finished = !actions.find((action) => action.status !== 'complete');
-      if(allActionFinished!==finished){
+
+      if (allActionFinished !== finished) {
         setAllActionFinished(finished);
       }
     }

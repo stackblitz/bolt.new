15bd5b24f3b847890e2d4273e5a9d2a534682773
Anirban Kar <thecodacus@gmail.com>
2024-12-08 02:11:47 +0530

    background rays  and changed the theme color to purple

--- app/components/ui/BackgroundRays/index.tsx
+++ app/components/ui/BackgroundRays/index.tsx
@@ -1,32 +1,6 @@
-// PurpleRays.jsx
-import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 
 const BackgroundRays = () => {
-  const [theme, setTheme] = useState('dark');
-
-  useEffect(() => {
-    // Initial theme
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    setTheme(currentTheme || 'dark');
-
-    // Optional: Watch for theme changes
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.attributeName === 'data-theme') {
-          const newTheme = document.documentElement.getAttribute('data-theme');
-          setTheme((existingTheme) => newTheme || existingTheme);
-        }
-      });
-    });
-
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ['data-theme'],
-    });
-
-    return () => observer.disconnect();
-  }, []);
   return (
     <div className={`${styles.rayContainer} `}>
       <div className={`${styles.lightRay} ${styles.ray1}`}></div>
